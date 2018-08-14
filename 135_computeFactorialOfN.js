80bf46acb4f80952f170a7cb069f65760022e47b
Christopher Dennis <cdennis.aus@gmail.com>
2018-08-14 14:11:29 -0500

    135-computeFactorialOfN.js

---
+++ 135_computeFactorialOfN.js
@@ -0,0 +1,23 @@
+function computeFactorialOfN(n) {
+  // let nums = [];
+  // for (var i  = 1; i <= n; i++) {
+  //   nums.push(i)
+  // }
+  // return nums.reduce((a, b) => a * b);
+
+  // OR
+
+  // var factorial = 1;
+  // for (var i = n; i > 1; i--) {
+  //   factorial *= i;
+  // }
+  // return factorial;
+
+  // OR
+
+  // let nums = [n];
+  // while (nums[0] > 1) {
+  //   nums.unshift(nums[0] - 1);
+  // }
+  // return nums.reduce((a, b) => a * b);
+}
