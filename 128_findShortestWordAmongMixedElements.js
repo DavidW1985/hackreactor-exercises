fb2dcd5a5fcc2dc9be956d6cbb2d05dc0c5c3be8
DavidW1985 <35374340+DavidW1985@users.noreply.github.com>
2019-04-25 10:13:44 +0200

    Update 128_findShortestWordAmongMixedElements.js

--- 128_findShortestWordAmongMixedElements.js
+++ 128_findShortestWordAmongMixedElements.js
@@ -1,4 +1,6 @@
 function findShortestWordAmongMixedElements(arr) {
   let words = arr.filter(word => typeof word === 'string');
-  return words.length > 0 ? words.sort((a, b) => b.length < a.length)[0] : '';
+  return words.length > 0 ? words.sort((a, b) => a.length - b.length)[0] : '';
 }
+
+
